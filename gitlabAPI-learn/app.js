ec95585e8aa144d7bc723aac4e8545cebe2808e4
nillworld <wwaosldk@gmail.com>
2022-07-25 20:38:20 +0900

    swagger with 'swagger-autogen'

--- gitlabAPI-learn/app.js
+++ gitlabAPI-learn/app.js
@@ -7,6 +7,9 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
+
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/signup", indexRouter);
 app.use("/users", usersRouter);
+
+//Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
